004a131d6809553d55c05601a52e56eda09ed910
NinjiClaw <andyv@inbox.ru>
2023-07-15 19:30:54 +0500

    done VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,35 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const vm = createApp({
+
+    data() {
+       return {
+           firstInputNumber: 0,
+           secondInputNumber: 0,
+           radioOperator: ''
+       };
+    },
+
+    computed: {
+        result() {
+            let outputtedNumber;
+
+            switch(this.radioOperator) {
+                case "sum":
+                  return outputtedNumber = this.firstInputNumber + this.secondInputNumber;
+                case "subtract":
+                    return outputtedNumber = this.firstInputNumber - this.secondInputNumber;
+                case "multiply":
+                    return outputtedNumber = this.firstInputNumber * this.secondInputNumber;
+                case "divide":
+                    return outputtedNumber = this.firstInputNumber / this.secondInputNumber;
+              }
+
+            return outputtedNumber;
+        }
+    },
+
+
+}).mount('#app');
