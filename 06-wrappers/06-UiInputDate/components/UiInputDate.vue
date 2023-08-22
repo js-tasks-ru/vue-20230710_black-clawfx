7c9c7c201278f56f1933c09c01ce9dbbd15e2fc0
NinjiClaw <andyv@inbox.ru>
2023-08-22 20:49:07 +0500

    06-wrappers 06-UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,5 +1,18 @@
 <template>
-  <UiInput />
+  <UiInput
+  v-bind="$attrs"
+  :type="type"
+  :model-value="formattedValue"
+  :step="step"
+  @input="$emit('update:modelValue', $event.target.valueAsNumber)"
+  >
+  <template v-if="$slots['left-icon']" #left-icon>
+     <slot name="left-icon"/>
+  </template>
+  <template v-if="$slots['right-icon']" #right-icon>
+     <slot name="right-icon"/>
+  </template>
+  </UiInput>
 </template>
 
 <script>
@@ -8,6 +21,47 @@ import UiInput from './UiInput.vue';
 export default {
   name: 'UiInputDate',
 
+  inheritAttrs: false,
+
   components: { UiInput },
+
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator: (value) => ['date', 'time', 'datetime-local'].includes(value),
+    },
+    modelValue: {
+      type: Number,
+      default: null
+    },
+    step: String,
+  },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    formattedValue() {
+
+      if (this.modelValue === null || this.modelValue === undefined) {
+        return "";
+      }
+
+      const date = new Date(this.modelValue);
+
+      switch (this.type) {
+        case 'date':
+        return date.toISOString().slice(0, 10);
+        case "time":
+        return this.step === '60' ? date.toISOString().slice(11, 19) : date.toISOString().slice(11, 16);
+        case "datetime-local":
+          return date.toISOString().slice(0, 16);
+        default:
+        return date.toISOString();
+      }
+    }
+  },
+
+
 };
 </script>
