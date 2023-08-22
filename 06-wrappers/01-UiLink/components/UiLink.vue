ba906491deb8d9a484da7925ddbbd99c695e0cc6
NinjiClaw <andyv@inbox.ru>
2023-08-22 22:39:28 +0500

    fix 06-wrappers 01-UiLink

--- 06-wrappers/01-UiLink/components/UiLink.vue
+++ 06-wrappers/01-UiLink/components/UiLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <component class="link" :is="tag" v-bind="$attrs">
+  <component class="link" :is="tag">
     <slot>Link!</slot>
   </component>
 </template>
@@ -7,11 +7,10 @@
 <script>
 export default {
   name: 'UiLink',
-  inheritAttrs: false,
 
   props: {
     tag: {
-      type:String,
+      type:[String, Object, Function],
       default: 'router-link',
     }
   }
