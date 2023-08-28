d3b8ad9e8d45d14c0095d6b8e1b470da98dab75c
NinjiClaw <andyv@inbox.ru>
2023-08-28 21:09:19 +0500

    08-reactivity 03-refHistory

--- 08-reactivity/03-refHistory/utils/refHistory.js
+++ 08-reactivity/03-refHistory/utils/refHistory.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 /**
  * @template T
@@ -6,7 +6,16 @@ import { ref } from 'vue';
  * @returns {Object<{ history: Ref<T[]> }>} - История изменения source
  */
 export function refHistory(source) {
-  const history = ref();
+  const history = ref([source.value]);
   // ...
+
+  watch(
+    source,
+    (newValue) => {
+      history.value.push(newValue);
+    },
+    { flush: 'sync' } // Добавляем опцию flush
+  );
+
   return { history };
 }
