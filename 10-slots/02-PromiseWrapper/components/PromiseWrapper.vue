6d22fc4b12cb51b84e90894c5284b75d2a9cbcb9
NinjiClaw <andyv@inbox.ru>
2023-09-01 19:03:53 +0500

    10-slots 02-PromiseWrapper

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,7 @@
 <template>
-  <!-- -->
+  <slot name="pending" v-if="isPending"></slot>
+    <slot v-else-if="isFulfilled" name="fulfilled" :result="result"></slot>
+    <slot v-else-if="isRejected" name="rejected" :error="error"></slot>
 </template>
 
 <script>
@@ -10,6 +12,55 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      isPending: true,
+      isFulfilled: false,
+      isRejected: false,
+      result: null,
+      error: null,
+    };
+  },
+
+  mounted() {
+    this.promise
+      .then((result) => {
+        this.isPending = false;
+        this.isFulfilled = true;
+        this.result = result;
+      })
+      .catch((error) => {
+        this.isPending = false;
+        this.isRejected = true;
+        this.error = error;
+      });
+  },
+
+  watch: {
+    promise: {
+      handler(newPromise) {
+        this.isPending = true;
+        this.isFulfilled = false;
+        this.isRejected = false;
+        this.result = null;
+        this.error = null;
+
+        newPromise
+          .then((result) => {
+            this.isPending = false;
+            this.isFulfilled = true;
+            this.result = result;
+          })
+          .catch((error) => {
+            this.isPending = false;
+            this.isRejected = true;
+            this.error = error;
+          });
+      },
+      immediate: true,
     },
   },
 };
