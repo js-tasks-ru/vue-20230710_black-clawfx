b1af239d56113ca70c4f8c2d6e91df749e358ff6
NinjiClaw <andyv@inbox.ru>
2023-09-07 13:40:59 +0500

    13-reusability 01-ProgressPlugin

--- 13-reusability/01-ProgressPlugin/plugins/progress/index.js
+++ 13-reusability/01-ProgressPlugin/plugins/progress/index.js
@@ -11,4 +11,38 @@ export function useProgress() {
 
 export function createProgress({ container, router } = {}) {
   // Решение
+  // Функция должна создать экземпляр TheTopProgressBar и смонтировать в элемент container
+  // Если container не передан, то createProgress должна сама создать контейнер для монтирования
+  const containerElement = document.createElement('div');
+  document.body.appendChild(containerElement);
+  const progress = createApp(TheTopProgressBar).mount(container || containerElement);
+
+
+  // Если передан router, прогресс должен интегрироваться с VueRouter:
+// Перед переходом на маршрут должен запускаться прогресс
+// После завершения перехода на маршрут должен завершаться прогресс
+// В случае ошибки при переходе должен прогресс должен завершаться неуспешно
+
+if (router) {
+  router.beforeEach((to, from, next) => {
+    progress.start(to.fullPath);
+    next();
+  });
+
+  router.afterEach(() => {
+    progress.finish();
+  });
+
+  router.onError(() => {
+    progress.fail();
+  });
+}
+// Добавляем метод инсталляции плагина
+progress.install = (app) => {
+  app.provide(PROGRESS_KEY, progress);
+};
+
+// Возвращаем объект progress
+return progress;
+
 }
